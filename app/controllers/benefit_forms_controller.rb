315ce815188ae8b46429293de2038745aeaab25c
Chris Aporta <christopher.aporta@flatironschool.com>
2016-05-26 16:45:53 -0400

    validate download params[:name] && params[:type] to secure app against unauthorized file downloads

--- app/controllers/benefit_forms_controller.rb
+++ app/controllers/benefit_forms_controller.rb
@@ -1,17 +1,26 @@
 class BenefitFormsController < ApplicationController
 
+  BASE_PATH = 'public/docs/'
+  VALID_FILES = ["#{BASE_PATH}Health_n_Stuff.pdf", "#{BASE_PATH}Dental_n_Stuff.pdf"]
+
+  def download
+    begin
+      if VALID_FILES.include?(params[:name]) && params[:type] == 'File'
+        path = Rails.root.join(params[:name])
+      else
+        path = Rails.root.join(VALID_FILES[0])
+      end
+      file = params[:type].constantize.new(path)
+      send_file file, disposition: 'attachment'
+    rescue
+      redirect_to user_benefit_forms_path(current_user.user_id)
+    end
+  end
+
   def index
     @benefits = Benefits.new
   end
 
-  def download
-   begin
-     file = params[:name]
-     send_file file, :disposition => 'attachment'
-   rescue
-     redirect_to user_benefit_forms_path(:user_id => current_user.user_id)
-   end
-  end
 
   def upload
     file = params[:benefits][:upload]
